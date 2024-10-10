2b164f184a2d32aa706c8b6fb3aa32bce052508a
Joel George <129869710+JoelGeorge04@users.noreply.github.com>
2024-10-10 22:25:10 +0530

    php-->

---
+++ ElectricityBill.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Electricity Bill Calculator</title>
+</head>
+<body>
+    <form method="post">
+        <label for="units">Units:</label>
+        <input id="units" type="number" name="units" required />
+        <br/><br/>
+        <input type="submit" name="submit" value="Calculate Bill"/>
+    </form>
+
+    <?php
+        if(isset($_POST['submit'])){
+            $units = $_POST['units'];
+            
+            function calculate($units){
+                $totalbill = 0.0;
+
+                if($units <= 50){
+                    $totalbill = $units * 3.50; // Direct rate
+                }
+                else if($units <= 100){
+                    $totalbill = (50 * 3.50) + (($units - 50) * 4.50);
+                }
+                else if($units <= 200){
+                    $totalbill = (50 * 3.50) + (50 * 4.50) + (($units - 100) * 5.50);
+                }
+                else{
+                    $totalbill = (50 * 3.50) + (50 * 4.50) + (100 * 5.50) + (($units - 200) * 6.50); 
+                }
+
+                return $totalbill;
+            }
+
+            $bill = calculate($units);
+            echo "<p>Total bill is $bill</p>";
+        }
+    ?>
+</body>
+</html>
