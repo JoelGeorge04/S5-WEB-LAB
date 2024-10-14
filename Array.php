b3ad2bdb7cf175b465b902cc1790e0b342f525a2
Joel George <129869710+JoelGeorge04@users.noreply.github.com>
2024-10-14 19:25:29 +0530

    Update Array.php

--- Array.php
+++ Array.php
@@ -1,11 +1,11 @@
 <html>
 <body>
 <?php
-    $cricket = array("Rohit", "Gill", "Kohli", "Bumrah");
-    echo "The Opener of the Team is $cricket[0] <br>";
-    echo "The Opener of the Team is $cricket[1] <br>";
-    echo "No 3 Of The Team is $cricket[2] <br>";
-    echo "Best bowler is $cricket[3] <br>";
+    $cricket = array("Rohit", "Sanju", "Kohli", "Sachin");
+    echo " -> $cricket[0] <br>";
+    echo " -> $cricket[1] <br>";
+    echo " -> $cricket[2] <br>";
+    echo " -> $cricket[3] <br>";
 ?>
 </body>
 </html>
