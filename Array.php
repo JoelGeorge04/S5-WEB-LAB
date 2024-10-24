eaa30d0dda260c17496515c279b24306b458b31b
Joel George <129869710+JoelGeorge04@users.noreply.github.com>
2024-10-24 19:12:00 +0530

    .php-->

--- Array.php
+++ Array.php
@@ -6,6 +6,8 @@
     echo " -> $cricket[1] <br>";
     echo " -> $cricket[2] <br>";
     echo " -> $cricket[3] <br>";
+
+    //print_r($cricket);
 ?>
 </body>
 </html>
---
+++ IndexedArray.php
@@ -0,0 +1,7 @@
+<?php
+    
+    echo "Indexed Array in PhP";
+    $car = array("bmw"=>"5-series","audi"=>"q5","merc"=>"s-class","tata"=>"nexon");
+    print_r($car);
+
+?>
